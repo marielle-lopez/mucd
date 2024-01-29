47ee0d132f1b5f157c58f84a5b96d859fa0640e6
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-01-29 22:36:56 +1100

    added Committee page to routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.scss";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
 import StylesPage from "./pages/StylesPage/StylesPage";
+import CommitteePage from "./pages/CommitteePage/CommitteePage";
 import NavBar from "./components/NavBar/NavBar";
 import Footer from "./components/Footer/Footer";
 
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/styles" element={<StylesPage />} />
+          <Route path="/committee" element={<CommitteePage />} />
         </Routes>
         <Footer />
       </BrowserRouter>

3826639347e431ea9f0b4704572c2bc3a449f4da
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-01-29 20:19:46 +1100

    added styling to IntroCard component

--- src/components/IntroCard/IntroCard.jsx
+++ src/components/IntroCard/IntroCard.jsx
@@ -1,16 +1,19 @@
 import styles from "./IntroCard.module.scss";
 import TextLink from "../TextLink/TextLink";
 
-const IntroCard = ({ title, text, links }) => {
+const IntroCard = ({ img, title, text, links }) => {
   return (
     <article className={styles.card}>
-      <h2>{title}</h2>
+      <img className={styles.card__img} src={img.path} alt={img.alt} />
+      <h2 className={styles.card__heading}>{title}</h2>
       {text.map((paragraph) => (
-        <p>{paragraph}</p>
+        <p className={styles.card__paragraph} key={paragraph}>
+          {paragraph}
+        </p>
       ))}
       <div className={styles.card__links}>
         {links.map((link) => (
-          <TextLink path={link.path} label={link.label} />
+          <TextLink key={link.path} path={link.path} label={link.label} />
         ))}
       </div>
     </article>

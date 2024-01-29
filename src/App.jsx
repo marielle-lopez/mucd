ee56c1164316971377c29d76570d1bc0c5e51204
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-01-29 20:48:59 +1100

    implemented Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
 import StylesPage from "./pages/StylesPage/StylesPage";
 import NavBar from "./components/NavBar/NavBar";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/styles" element={<StylesPage />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </>
   );

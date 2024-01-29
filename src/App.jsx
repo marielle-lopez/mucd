f950092d77a8c1d9922bd20c41da6457e9c81449
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-01-29 18:42:01 +1100

    created, styled, and implemented NavBar component

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import "./App.scss";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
 import StylesPage from "./pages/StylesPage/StylesPage";
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <NavBar />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/styles" element={<StylesPage />} />
